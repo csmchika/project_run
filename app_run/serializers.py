73f4db15dea35bb7efcf5203c8851457a8f9b9ec
Alex5 <Alex555dog@mail.ru>
2026-07-09 16:46:59 +0300

    fourth_task_test

--- app_run/serializers.py
+++ app_run/serializers.py
@@ -2,7 +2,15 @@ from rest_framework import serializers
 from django.contrib.auth.models import User
 from .models import Run
 
+class AthleteSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'last_name', 'first_name',]
+
+
 class RunSerializer(serializers.ModelSerializer):
+    athlete_data = AthleteSerializer(source="athlete", read_only=True)
     class Meta:
         model = Run
         fields = '__all__'
